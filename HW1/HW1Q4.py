34adfb2a6356166f44ada2a04ebfedbe06192584
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2023-02-20 15:28:50 +0300

    Update HW1/HW1Q4.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- HW1/HW1Q4.py
+++ HW1/HW1Q4.py
@@ -1,7 +1,7 @@
 n = int(input('n = '))
 m = int(input('m = '))
 k = int(input('k = '))
-if k < m*n and k % m == 0 or k < m*n and k % n == 0:
+if k < m * n and (k % m == 0 or k % n == 0):
     print('yas')
 else:
     print('no')
