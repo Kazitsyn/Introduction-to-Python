42dc3f83d7690d80107e8d0d62886ec8b3286ed1
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2023-02-24 19:39:12 +0300

    HW2Q1 compcompleted

--- HW2/HW2Q1.py
+++ HW2/HW2Q1.py
@@ -3,3 +3,21 @@
 # чтобы все монетки были повернуты вверх одной и той же стороной. 
 # Выведите минимальное количество монет, которые нужно перевернуть
 
+import random
+
+n = int(input('n = '))
+coin = (['eagle', 'tail'])
+table = []
+while n > 0:
+    table.append(random.choice(coin))
+    n -= 1
+print(table)
+
+eagle = tail = 0
+for i in table:
+    if i == 'eagle':
+        eagle += 1
+    else:
+        tail += 1
+        
+print(eagle if eagle <= tail else tail)
