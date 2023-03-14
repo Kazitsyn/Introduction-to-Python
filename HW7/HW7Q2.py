8ceb2155e59e7921a53e6ae6d1c450fa3ac9d595
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2023-03-14 20:34:10 +0300

    add q2

--- HW7/HW7Q2.py
+++ HW7/HW7Q2.py
@@ -7,7 +7,14 @@
 """
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
+    row = [] 
+    matrix = [row]
+    for i in range(1, num_rows + 1):
+        for j in range(1, num_columns + 1):
+            row.append(operation(i,j))
+    for row in matrix:
+        print(' '.join(list(map(str, row))))
 
-    pass
+    
 
 print_operation_table(lambda x, y: x * y)
